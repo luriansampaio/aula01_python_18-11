3dbb25d08ab37fe552b3285a6f2de89186440cf3
luriansampaio <luriansampaio@gmail.com>
2025-11-18 19:43:03 -0300

    inicia o repositório da Aula 01

---
+++ aula01/exemplo.py
@@ -0,0 +1,74 @@
+# nome = 'Ana'
+# idade = 25
+# salario = 15000.0
+
+# #saída
+# print(f'O nome dela é {nome}, \nidade {idade} e o \nsalario {salario}')
+
+# x = 5
+# x = '10'
+# print(x)
+
+
+# nome = input('Digite o nome: ')
+# print(f'Olá, {nome}!!')
+
+# cor_favorita = input('Digite a cor: ')
+# print(f'Sua cor favorita é: {cor_favorita}?')
+
+# n1 =float(input('Digite qualquer valor:'))
+# n2 = float(input('Digite outro valor:'))
+# resultado = n1 + n2
+# print(f'o resultado foi: {resultado}')
+
+# nome_usuario = input('Informe o nome')
+# idade_usuario = input('Informe a idade')
+# print(f'O nome é {nome_usuario} e a idade é {idade_usuario}')
+
+# nome_da_cidade = input('Digite a cidade:')
+# nome_do_bairro = input('Digite o bairro:')
+# print(f'o nome da cidade é {nome_da_cidade} e o bairro é {nome_do_bairro}')
+
+# num1 = float(input('Informe algum número:'))
+# num2 = float(input('Informe outro número:'))
+# resultado = num1 * num2
+# print(resultado)
+
+# idade_do_usuario = int(input('Informe a idade:'))
+# resultado = 2025 - (idade_do_usuario)
+# print(f'O seu ano de nascimento é {resultado}')
+
+# dinheiro_no_banco = (450)
+# valor_do_ingresso = 37
+# resultado = (dinheiro_no_banco) / (valor_do_ingresso)
+# print(f'É possível comprar {resultado} ingressos')
+
+# dinheiro_disponível = float(input('Digite o valor obtido aqui:'))
+# valor_do_ingresso = float(input('Digite aqui o valor do ingresso:'))
+# resultado = (dinheiro_disponível) / (valor_do_ingresso)
+# print(f'É possível comprar {resultado} ingressos')
+                            
+# preço = float(input(f'Informe o preço:'))
+# quantidade = int(input(f'Informe a quantidade:'))
+# total = preço * quantidade 
+# desconto = total * 10 / 100 
+# valor_com_desconto = total - desconto
+# print(f'valor total {total}\nDesconto de 10% {desconto}\nValor a pagar:{valor_com_desconto:.2f}')
+
+salario_do_usuario = float(input('Digite o valor:'))
+acrescimo = salario_do_usuario * 0.15
+resultado = salario_do_usuario + acrescimo 
+print(f'O salário subiu para {resultado}.')
+
+
+
+
+
+
+
+
+
+
+
+
+
